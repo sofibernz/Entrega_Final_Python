1360f2101872c20f7ce609a714e962953d9e8b46
Sofia Bernz <sofibernz@gmail.com>
2021-12-20 23:21:41 -0300

    Version 3. Modificacion de models.

--- ProyectoFinal_Python/PreEntregaCoder/AppCoder/models.py
+++ ProyectoFinal_Python/PreEntregaCoder/AppCoder/models.py
@@ -6,6 +6,10 @@ class Productos(models.Model):
 
     nombreProd = models.CharField(max_length=40)
     precio = models.IntegerField()
+
+    def __str__(self):
+
+        return f"{self.nombreProd} - ${self.precio}"
     
 class Clientes(models.Model):
 
@@ -13,9 +17,17 @@ class Clientes(models.Model):
     apellido = models.CharField(max_length=40)
     codCliente = models.IntegerField()
     email = models.EmailField()
+
+    def __str__(self):
+
+        return f"Código: {self.codCliente} - Apellido y Nombre: {self.apellido}, {self.nombre} - Email: {self.email}"
     
 class Vendedores(models.Model):
 
     nombre = models.CharField(max_length=40)
     apellido = models.CharField(max_length=40)
     jerarquia = models.CharField(max_length=30)
+
+    def __str__(self):
+
+        return f"{self.apellido}, {self.nombre} - {self.jerarquia}"
